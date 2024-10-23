63af07ba0ffc2ded49a5af9c2f6cd1e89df88111
Fabio Aragao <fabio_aragao@yahoo.com.br>
2024-10-22 21:02:38 -0300

    Arquivos modificados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Testando o Nokia
+        Smartphone nokia = new Nokia("123456", "Nokia Model 1", "111111111111", 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        // Testando o iPhone
+        Smartphone iphone = new Iphone("654321", "iPhone Model X", "222222222222", 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
